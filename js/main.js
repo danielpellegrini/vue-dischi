c661f1ea09c665813f34d3b4db0ad7d5acfe7148
Daniel <danielpellegrini87@gmail.com>
2021-02-08 15:41:03 +0100

    select and v-if added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 // Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
 // Stampiamo tutto a schermo.
 
+// Bonus: Creare una select con i seguenti generi:
+// pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+
 var app = new Vue({
   el: '#app',
   data: {
 
-    songArray: []
+    songArray: [],
+    selected: ''
 
   },
 
